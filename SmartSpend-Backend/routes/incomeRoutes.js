8c4e477dc2588971bb291ead4233e582db2f3877
amarbazlin <aamarbazlin@gmail.com>
2025-07-27 11:31:06 +0530

    make all the new changes

--- SmartSpend-Backend/routes/incomeRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const incomeController = require("../controllers/incomeController");
-
-router.post("/", incomeController.createIncome);
-router.get("/:userId", incomeController.getUserIncome);
-
-module.exports = router;
